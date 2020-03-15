40e94ffef5d478a7f2f3a2e8e5cb4579cf795776
no134340 <jasminasavic98@gmail.com>
2020-03-15 19:38:27 +0100

    changes

--- hw1/merge_module.py
+++ hw1/merge_module.py
@@ -1,31 +1,27 @@
 import math
 
 
-def merge(left, right):
-    arr = []
-    n = len(right) + len(left)
+def merge(arr, p, q, r):
+    left = list(arr[p:r + 1])
     left.append(math.inf)
+    right = list(arr[r + 1:q + 1])
     right.append(math.inf)
     lind, rind = 0, 0
-    for i in range(n):
+    for i in range(p, q + 1):
         if left[lind] > right[rind]:
-            arr.append(right[rind])
+            arr[i] = right[rind]
             rind += 1
         else:
-            arr.append(left[lind])
+            arr[i] = left[lind]
             lind += 1
-    return arr
 
 
-def merge_sort(arr):
-    if len(arr) > 1:
-        div = len(arr) // 2
-        left_array = merge_sort(arr[:div])
-        right_array = merge_sort(arr[div:])
-        sorted_arr = merge(left_array, right_array)
-        return sorted_arr
-    else:
-        return arr
+def merge_sort(arr, p, q):
+    if p < q:
+        div = (q + p) // 2
+        merge_sort(arr, p, div)
+        merge_sort(arr, div + 1, q)
+        merge(arr, p, q, div)
 
 
 if __name__ == '__main__':
@@ -33,8 +29,8 @@ if __name__ == '__main__':
     import sys
 
     print(sys.getrecursionlimit())
-    arr_range = 10
+    arr_range = 200
     test_arr = [random.randint(1, 20) for x in range(arr_range)]
     print("The original array is: {}".format(test_arr))
-    test_arr = merge_sort(test_arr)
+    merge_sort(test_arr, 0, len(test_arr) - 1)
     print("The sorted array is: {}".format(test_arr))
